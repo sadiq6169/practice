fcf047d5125a96f31c736b8785f11854312a07be
sadiq6169 <sadiq6169@gmail.com>
2023-10-06 21:45:11 +0500

    javascript folder

---
+++ javascript/ap.js
@@ -0,0 +1,157 @@
+// let firstName = "sadiq"
+// firstName = "khan"
+// console.log(firstName)
+
+// ----------------Assignment Operator(=)---------
+// var x = 5;
+// var y = 5;
+
+// console.log('is both the x and y are equal or not' + x ==y);
+
+// console.log(`is both the x and y are equal or not : ${x ==y}`);
+
+
+// -------------------Arithmetic Operators(+,-,*,/)-----------
+
+// console.log(2+4)
+// console.log(3*5)
+// console.log(25/5)
+// console.log(7-4)
+
+// console.log('Reminder Operator is ' +  81%8)
+
+// -----------increment & decrement operator---------------
+
+// var num = 10;
+// var newNum = num++ +5;
+// console.log(num);
+// console.log(newNum)
+
+// var num = 10;
+// var newNum = ++num + 5;
+// console.log(num);
+// console.log(newNum)
+
+
+// var num = 10;
+// var newNum = num-- +5;
+// console.log(num);
+// console.log(newNum)
+
+
+// var num = 10;
+// var newNum = --num +5;
+// console.log(num);
+// console.log(newNum)
+
+// --------------comparsion Operator--------
+
+// var a = 10;
+// var b = 20;
+// console.log(a == b)
+// console.log(a !=b )
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a >= b)
+// console.log(a <= b)
+
+
+// ------------------logical Operator(&, ||, !)----------
+
+// var a = 10;
+// var b = 5;
+
+// console.log(a > b && b < a);
+// console.log(a > b && b > a && a >= b);
+
+// console.log(a > b || b > a || a >= b)
+
+// console.log(!(a > b) || (b > a) || (a >= b))
+// console.log(!true)
+// console.log(!false)
+
+
+// ----------------IF , ELSE------------------
+
+// var year = 2023;
+// if (year % 4 ===0){
+//     if(year % 100 ===0){
+//         if(year % 400 ===0){
+
+//         console.log('the year is' + year + 'is a leaf year');
+//     }else{
+//         console.log('The year is ' + year + ' is not leaf year');
+//          }
+//         }else{
+//             console.log('The year is ' + year + 'is a leaf year');
+//         }
+//     }else{
+//         console.log('The year is ' + year + 'is not leaf year');
+
+//     }
+
+        // var age = 17;
+        // if (age >=18){
+        //     console.log("you are eligible to vote");
+        // }else{
+        //     console.log("you are not eligible to vote");
+        // }
+    
+// --------------------conditional Ternary Operator--------------
+
+//     var age = 19;
+//     console.log((age >= 18) ? "your are eligible" : "you can't eligible") 
+
+
+// -----------------while loop---------------------
+// es mai phle condition dake jate hai pir loop chalta hai//
+
+// var num = 0;
+//block scope
+// while(num <=10){
+//         console.log(num);
+//         num++;
+// }
+
+
+// ---------------DO WHILE LOOP-----------------------
+//es mai phle loop chalta hai pir condition dakte hai//
+
+// var num = 0;
+//      do{
+//         console.log(num);
+//         num++;
+// }while(num <= 10);
+
+//---------FOR LOOP (Initialization , condition , iteration)-------
+
+// for (var num = 0; num <= 10; num++){
+//         console.log(num);
+// }
+
+
+for (var num = 1; num <= 10; num++){
+        var table = 8;
+    console.log(table  + "*" + num + "=" + table*num)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/app.js
@@ -0,0 +1,149 @@
+// document.write("abcd")
+// alert("xyz")
+// console.log("sadiq")
+
+// var street;
+// var country;
+// street = "2";
+// country = "Pakistan";
+// var FullMailingAddress =  "street + country";
+
+
+// var score1 = 20;
+// var score2 = 20;
+// var score3 = 20;
+// var totalscore = score1 + score2 + score3
+// var averagescore = totalscore/3
+// console.log(totalscore, averagescore)
+// var plate = 20;
+// var people = 7;
+// var remainingPlate = plate % people
+// console.log(remainingPlate)
+// remainingPlate +=1;
+// console.log("the remaining plates are" + remainingPlate)
+
+
+// var number = prompt("konse se table likna hai");
+// console.log(number + "* 1 =" +(number*1))
+// console.log(number + "* 2 =" +(number*2))
+// console.log(number + "* 3 =" +(number*3))
+// console.log(number + "* 4 =" +(number*4))
+// console.log(number + "* 5 =" +(number*5))
+// console.log(number + "* 6 =" +(number*6))
+// console.log(number + "* 7 =" +(number*7))
+// console.log(number + "* 8 =" +(number*8))
+// console.log(number + "* 9 =" +(number*9))
+// console.log(number + "* 10 =" +(number*10))
+
+// var fruits=["mango", "orange", "banana", "grapes"]
+
+// fruits[3] = "kuch bhi"
+
+// console.log(fruits)
+
+// Juice Maker Shop......
+// function juicebananewala(fruitjuice){
+//     console.log("jao our ja k deko")
+//     console.log("customer ko kiya chaiye")
+//     console.log("water: sir ap ko konse juice chaiye")
+//     console.log("customer: muje" + fruitjuice + "ka juice chaiye")
+//     console.log("customer ko ak" + fruitjuice + "juice chaiye")
+//     console.log("jao our do" + fruitjuice + "lo our un ko pilao")
+//     console.log("or pir" + fruitjuice + "ka pieces karo ")
+//     console.log("pir" + fruitjuice + "fruit ko machine me dalo")
+//     console.log("jab" + fruitjuice + "juice bana do")
+//     console.log("tu ak glass lo" + fruitjuice + "juice ko us mai dal do")
+//     console.log("customer ko da do")
+// }
+
+//     juicebananewala(prompt("janab konse sa juice chaiya"))
+//     juicebananewala("Apple")
+//     console.log("===========================")
+//     juicebananewala("Mango")
+//     console.log("===========================")
+//     juicebananewala("Banana")
+//     console.log("===========================") 
+    
+
+    // var fruits = ["mango", "apple", "orange", "banana", "grapes"]
+    // console.log("fruits.length", "=======>> lenght")
+    // fruits["fruits.length"]
+    // console.log(fruits)
+
+//  function CalculateTotal(subTotal,tax){
+//     return subTotal + tax;
+// }
+//  let order1,order2,order3;
+//  function calculateResult(a,b,c) {
+//     order1 = a;
+//     order2 = b;
+//     order3 = c;
+//     console.log(order1,order2,order3);
+//  }
+ 
+ 
+//  calculateResult(10,20,30);
+
+
+// var a = 4;
+// if(++a === 5){
+//    alert("given condition for variable is true");
+// }
+
+
+// var b = 82;
+// if(b++ === 83){
+//    alert("given condition for variable is true");
+// }
+
+// var c = 12;
+// if(c++ === 13){
+//    alert("condition 1 is true");
+// }
+
+// if( c=== 13){
+//    alert("condition 2 is true");
+// }
+
+// if( ++c < 14){
+//    alert("condition 3 is true");
+// }
+// if( c=== 14){
+//    alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalcost = materialCost + laborCost;
+// if(totalcost === laborCost + materialCost){
+//    alert("The cost equal");
+// }
+
+// if(true){
+//    alert("True");
+// }
+
+// if(false){
+//    alert("False");
+// }
+
+// if("car" < "car"){
+//    alert("car is smaller than cat")
+// }
+
+
+   var car = {
+   make : 'suzuki',
+   model : 'VXR',
+   year : '2015',
+   colors : ['black' , 'red' , 'white'],
+   hybrid : true,
+   drive : function Drive(){
+      console.log("car chal rahe hai")
+   },
+   stop : function Stop(){
+      console.log("car ruk gai")
+   },
+}
+
+
