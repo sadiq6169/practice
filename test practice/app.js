ac8d061008067b597d0f9313ae4512b421643d66
sadiq6169 <sadiq6169@gmail.com>
2023-10-11 16:25:24 +0500

    test

---
+++ test practice/app.js
@@ -0,0 +1,50 @@
+// var scoreAvg =math.round( 1.5)
+// var score = math.round(scoreAvg)
+// console.log(scoreAvg)
+
+// var currentDate = new Date();
+// console.log(currentDate);
+// const year = currentDate.getFullYear();
+// const month = currentDate.getMonth();
+// const day = currentDate.getDay();
+// const hours = currentDate.getHours();
+// const minutes = currentDate.getMinutes();
+// const seconds = currentDate.getSeconds();
+
+// const currentDate = new Date();
+
+// // Get the full date (e.g., "Sat Oct 08 2023 15:45:30 GMT+0000 (Coordinated Universal Time)")
+// console.log(currentDate);
+
+// // Get the year, month, day, and time separately
+// const year = currentDate.getFullYear();
+// const month = currentDate.getMonth(); // Note: Months are zero-based (0 - 11)
+// const day = currentDate.getDate();
+// const hours = currentDate.getHours();
+// const minutes = currentDate.getMinutes();
+// const seconds = currentDate.getSeconds();
+
+// // Format the date as a string (e.g., "2023-10-08 15:45:30")
+// const formattedDate = `${year}-${(month + 1).toString().padStart(2, '0')}-${day.toString().padStart(2, '0')} ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+// console.log(formattedDate);
+
+// var num = 1.95
+// // num = Math.ceil(num);
+// num = Math.floor(num);
+
+// var d = new Date();
+//     d.setFullYear(2023);
+
+//  var d = new Date();
+//      d.setDate(11);
+
+
+     var d = new Date();
+         d.setHours(15);
+
+
+
+
+
+
+
